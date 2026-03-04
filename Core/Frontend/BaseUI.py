5760c24926d5eada3fe3c27005032767e5c1ec21
WASDDestroy <3288211880@qq.com>
2026-03-04 14:51:58 +0800

    Improved method handleBackAndExit(), it will return boolean to help outer program understand "I should stop processing consequent logic"

--- Core/Frontend/BaseUI.py
+++ Core/Frontend/BaseUI.py
@@ -4,7 +4,7 @@ import sys
 
 class BaseUI:
 
-    def __init__(self, logger = None, gotoNode = "") -> None:
+    def __init__(self, logger = None, gotoNode = "", navigationEngine = None) -> None:
         self.TAG = "BaseUI"
         self.nodeFunction = {}
         self.customizedFunction = {} # "Press Key" : "Function Name"
@@ -32,7 +32,7 @@ class BaseUI:
         except ImportError as e:
             print(e)
         self.myLogger = logger or LogUtils.LogUtils() # type: ignore
-        self.myNavigationEngine = NavigationEngine.NavigationEngine(self.myLogger) # type: ignore
+        self.myNavigationEngine = navigationEngine or NavigationEngine.NavigationEngine(self.myLogger) # type: ignore
         if gotoNode:
             self.myNavigationEngine.gotoNode(gotoNode)
         self.customizedInit()
@@ -82,6 +82,7 @@ class BaseUI:
     def _createInstance(self, module, className, logger):
         try:
             ClassName = getattr(module, className)
+            self.myLogger.log("I", "Successfully get attribute in module %s with class name %s"%(module, className), self.TAG)
         except AttributeError:
             self.myLogger.log("E", "Failed when creating instance for module with class name %s, no such attribution!"%(className), self.TAG)
             raise RuntimeError("Unable to create instance.")
@@ -91,6 +92,7 @@ class BaseUI:
     def _createFrontendInstance(self, module, className, logger, gotoNode):
         try:
             ClassName = getattr(module, className)
+            self.myLogger.log("I", "Successfully get attribute in module %s with class name %s"%(module, className), self.TAG)
         except AttributeError:
             self.myLogger.log("E", "Failed when creating instance for module with class name %s, no such attribution!"%(className), self.TAG)
             raise RuntimeError("Unable to create instance.")
@@ -99,6 +101,7 @@ class BaseUI:
 
     def getNodeFunctions(self):
         # Node function = Next nodes + Customized actions
+        self.nodeFunction = {}
         for i in self.customizedFunction:
             self.nodeFunction[i] = self.customizedFunction[i]
         nextNodesDict = self.myNavigationEngine.getNextNodeNames()
@@ -111,16 +114,20 @@ class BaseUI:
     
     def handleBackAndExit(self, functionName):
         if "back" in functionName.lower():
+            self.myLogger.log("I", "Back to upper level.", self.TAG)
             self.myNavigationEngine.goToUpperLevel()
-            return
+            self.myNavigationEngine.refreshNodeInfo()
+            self.myLogger.log("I", "Currently using file: " + self.myNavigationEngine.currentFileDir, self.TAG)
+            return True
         if functionName == "Exit":
             print("Exiting.")
             self.myLogger.log("I", "Exit on UI request.", self.TAG)
             exit()
+        return False
 
     def callBackEnd(self, functionName : str):
         self.handleBackAndExit(functionName)
-        raise NotImplementedError("Unimplemented method callBackEnd. Module: " + self.TAG)
+        raise NotImplementedError("Unimplemented method callBackEnd." + self.TAG)
 
     def confirmOperation(self, prompt = "Confirm operation?") -> bool:
         try:
@@ -153,36 +160,51 @@ class BaseUI:
     
     def handleInteractionLogic(self):
         mySelection = input("Your choice: ").upper()
-        self.myLogger.log("V", "User input: " + mySelection, self.TAG)
+        self.myLogger.log("T", "User input: " + mySelection, self.TAG)
         if not mySelection in self.nodeFunction.keys():
             print("No such choice.")
             self.myLogger.log("W", "Illegal choice: " + mySelection, self.TAG)
+            self._pressEnterToContinue()
         else:
             functionName = self.nodeFunction[mySelection]
+            self.myLogger.log("T", "Function name: " + functionName, self.TAG)
             # Check whether function is in next node
             if self.myNavigationEngine.currentDic["Next"][0] != "END":
+                self.myLogger.log("T", "Current node has subnodes, traverse them.", self.TAG)
                 for i in self.myNavigationEngine.currentDic["Next"]:
+                    self.myLogger.log("T", "Traversing, current: " + i, self.TAG)
                     self.myNavigationEngine.gotoNode(i)
                     self.myNavigationEngine.refreshNodeInfo()
+                    self.myLogger.log("T", "Switched node and refreshed info.", self.TAG)
                     if self.myNavigationEngine.currentDic["Name"] == functionName:
                         # Found function in one of the next node, dynamically import it and execute entry
                         moduleName = self.myNavigationEngine.currentDic["Frontend"].rstrip(".py")
-                        self.myLogger.log("V", "Navigate to: " + moduleName, self.TAG)
+                        self.myLogger.log("T", "Found function! Corresponding module name: " + moduleName, self.TAG)
+                        self.myLogger.log("T", "Navigate to: " + moduleName, self.TAG)
                         myObject = self._createFrontendInstance(self._importFrontEndModule(moduleName),
                                                         moduleName,
                                                         self.myLogger,
                                                         i)
-                        myObject.entry()
+                        myObject.entry(navigationEngine = self.myNavigationEngine)
                         break
                     else:
+                        self.myLogger.log("T", "Function name mismatch, go to upper level.", self.TAG)
                         self.myNavigationEngine.goToUpperLevel()
                 else:
                     # If the loop ends normally, call functions in current node.
+                    self.myLogger.log("T", "Traverse end! Target function not found!", self.TAG)
+                    self.myLogger.log("T", "Call current node's function by name: " + functionName, self.TAG)
                     self.callBackEnd(functionName)
             else:
+                self.myLogger.log("T", "Current node does not contain subnodes, directly call function: " + functionName, self.TAG)
                 self.callBackEnd(functionName)
         
-    def entry(self):
+    def entry(self, navigationEngine = None):
+        if navigationEngine is not None:
+            self.myLogger.log("D", "Use provided navigation engine.", self.TAG)
+            self.myNavigationEngine = navigationEngine
+        self.myLogger.log("D", self.myNavigationEngine.currentNodeName, self.TAG)
+        self.myLogger.log("D", self.myNavigationEngine.currentNodeNext, self.TAG)
         self.showUI()
         self.handleInteractionLogic()
 
