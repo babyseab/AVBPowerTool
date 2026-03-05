79395bddf1dc1e8262882b77820176cf0eec6d75
WASDDestroy <3288211880@qq.com>
2026-03-05 23:52:16 +0800

    Add image name display in test.py when traversing test directory

--- Core/test.py
+++ Core/test.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 
 for i in os.listdir("F:\\testImages"):
+    print("Image name: %s"%(i))
     result = subprocess.run(["python",
                              "F:\\GitHub\\AVBPowerTool\\Core\\avbtool.py",
                              "info_image",
