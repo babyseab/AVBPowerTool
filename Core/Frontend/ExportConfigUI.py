9892cef47a9190f802cf9f3a97480ae5453f8c72
WASDDestroy <3288211880@qq.com>
2026-03-13 23:54:27 +0800

    Extract more frontend logic from __call_export_backend()

--- Core/Frontend/ExportConfigUI.py
+++ Core/Frontend/ExportConfigUI.py
@@ -29,19 +29,27 @@ class ExportConfigUI(BaseUI.BaseUI):
         my_file_selector = EnhancedFileSelectorUI(title="Select a Config", items=file_can_be_selected,
                                                   multi_select=True, logger=self.my_logger)
         config_list = my_file_selector.show()
+        export_result = False
         if len(config_list) == 0:
             print("User cancelled operation.")
+            self.my_ui_utils.press_enter_to_continue()
             return
         elif len(config_list) > 1:
             if self.confirm_operation("Should export these configs as sparse archives?"):
                 for i in config_list:
-                    self.__call_export_backend(i, True)
+                    export_result = self.__call_export_backend(i, True)
             else:
-                self.__call_export_backend(sparse=False, config_list=config_list)
+                export_result = self.__call_export_backend(sparse=False, config_list=config_list)
         else:
             config_name = config_list[0]
-            self.__call_export_backend(config_name, True)
-
+            export_result = self.__call_export_backend(config_name, True)
+        if export_result:
+            print("Successfully exported selected config to root directory as an archive.")
+            print("Config(s) exported:")
+            for config_name in config_list:
+                print(config_name)
+        else:
+            print("Failed to export config!")
 
         self.my_ui_utils.press_enter_to_continue()
 
@@ -59,13 +67,9 @@ class ExportConfigUI(BaseUI.BaseUI):
                                                                                       + time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
                                                                                       + ".zip",
                                                                   selected_configs=config_list)
-            if result:
-                print("Successfully exported selected config %s to root directory as an archive." % (
-                    config_name))
-            else:
-                print("Failed to export config!")
+            return result
         except FileNotFoundError:
-            print("Config folder not found!")
             self.my_logger.log("W",
                                "Config folder not found! Check system settings because config is already guaranteed exist in previous steps.",
                                self.TAG)
+            return False
